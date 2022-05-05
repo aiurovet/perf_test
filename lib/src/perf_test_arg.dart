bb76fc19e642549d9ee4fdea432835fe1e9b4619
aiurovet <aiurovet@gmail.com>
2022-05-05 22:49:20 +1000

    PerfTestArg - WIP

--- lib/src/perf_test_arg.dart
+++ lib/src/perf_test_arg.dart
@@ -6,6 +6,46 @@ import 'package:perf_test/perf_test.dart';
 /// Generic argument to generate any kind of test data
 ///
 class PerfTestArg {
+  /// The default iterable value
+  ///
+  static final defaultIterable = [];
+
+  /// The default step for DateTime value
+  ///
+  static final int defaultStepForInt = 1;
+
+  /// Default step for DateTime value
+  ///
+  static final num defaultStepForNum = 1;
+
+  /// Default step for DateTime value
+  ///
+  static final Duration defaultStepForDateTime = Duration(days: 1);
+
+  /// Maximum DateTime value
+  ///
+  static final DateTime maxDateTime = DateTime.utc(275760, 09, 13);
+
+  /// Minimum DateTime value
+  ///
+  static final DateTime minDateTime = DateTime.utc(-271821, 04, 20);
+
+  /// Maximum int value
+  ///
+  static const int maxInt =  9007199254740991;
+
+  /// Minimum int value
+  ///
+  static const int minInt = -9007199254740992;
+
+  /// Maximum num value
+  ///
+  static const num maxNum =  9223372036854775807;
+
+  /// Minimum num value
+  ///
+  static const num minNum = -9223372036854775808;
+
   /// The maximum value of a range
   ///
   Object? current;
@@ -20,36 +60,68 @@ class PerfTestArg {
 
   /// The list of (non-unique) values
   ///
-  final Iterable? iterable;
+  Iterable iterable = defaultIterable;
 
-  /// The minimum value of a range
+  /// Maximum DateTime value of a range
   ///
-  final Object? min;
+  DateTime dateTimeMax = maxDateTime;
 
-  /// The maximum value of a range
+  /// Minimum DateTime value of a range
   ///
-  final Object? max;
+  DateTime dateTimeMin = minDateTime;
 
-  /// The step of a range
+  /// Step for the DateTime range
   ///
-  final Object? step;
+  Duration dateTimeStep = defaultStepForDateTime;
+
+  /// Maximum int value of a range
+  ///
+  int intMax = maxInt;
+
+  /// Minimum int value of a range
+  ///
+  int intMin = minInt;
+
+  /// Step for the int range
+  ///
+  int intStep = defaultStepForInt;
+
+  /// Maximum num value of a range
+  ///
+  num numMax = maxNum;
+
+  /// Minimum num value of a range
+  ///
+  num numMin = minNum;
+
+  /// Step for the num range
+  ///
+  num numStep = defaultStepForNum;
 
   /// The constructor
   ///
-  PerfTestArg({this.iterable, this.min, this.max, this.step}) {
-    type = (
-      (iterable != null) ? PerfTestArgType.iterable :
-      (min is int) ||
-      (max is int) ||
-      (step is int) ? PerfTestArgType.int :
-      (min is num) ||
-      (max is num) ||
-      (step is num) ? PerfTestArgType.num :
-      (min is DateTime) ||
-      (max is DateTime) ||
-      (step is Duration) ? PerfTestArgType.dateTime :
-      PerfTestArgType.unknown
-    );
+  PerfTestArg({Iterable? iterable, Object? min, Object? max, Object? step}) {
+    if (iterable != null) {
+      type = PerfTestArgType.iterable;
+      this.iterable = iterable;
+    } else if ((min is int) || (max is int) || (step is int)) {
+      type = PerfTestArgType.int;
+      intMin = ((min as int?)  ?? minInt);
+      intMax = ((max as int?)  ?? maxInt);
+      intStep = ((step as int?) ?? defaultStepForInt);
+    } else if ((min is num) || (max is num) || (step is num)) {
+      type = PerfTestArgType.num;
+      numMin = ((min as num?)  ?? minNum);
+      numMax = ((max as num?)  ?? maxNum);
+      numStep = ((step as int?) ?? defaultStepForInt);
+    } else if ((min is DateTime) || (max is DateTime) || (step is Duration)) {
+      type = PerfTestArgType.num;
+      dateTimeMin = ((min as DateTime?) ?? minDateTime);
+      dateTimeMax = ((max as DateTime?) ?? maxDateTime);
+      dateTimeStep = ((step as Duration?) ?? defaultStepForDateTime);
+    } else {
+      type = PerfTestArgType.unknown;
+    }
 
     reset();
   }
@@ -57,78 +129,47 @@ class PerfTestArg {
   Object? moveNext() {
     switch (type) {
       case PerfTestArgType.dateTime:
-        current = moveNextDateTime(current as DateTime, min as DateTime?, max as DateTime?, step as Duration?);
+        var cur = current as DateTime;
+        isFinished = (cur == dateTimeMax);
+        current = (isFinished ? dateTimeMin : cur.add(dateTimeStep));
         break;
       case PerfTestArgType.int:
-        current = moveNextInt(current as int, min as int?, max as int?, step as int?);
+        var cur = current as int;
+        isFinished = (cur == intMax);
+        current = (isFinished ? intMin : cur + intStep);
         break;
       case PerfTestArgType.iterable:
-        current = moveNextIterable(iterable!);
+        final itr = iterable.iterator;
+        current = (itr.moveNext() ? itr.current : iterable.first);
         break;
       case PerfTestArgType.num:
-        current = moveNextNum(current as num, min as num?, max as num?, step as num?);
+        var cur = current as num;
+        isFinished = (cur == numMax);
+        current = (isFinished ? numMin : cur + numStep);
         break;
       default:
         current = null;
     }
 
-    if (iterable != null) {
-    } else if ((min is int) || (max is int) || (step is int)) {
-      current = moveNextInt(current as int, min as int?, max as int?, step as int?);
-    } else if ((min is num) || (max is int) || (step is int)) {
-      current = moveNextNum(current as num, min as num?, max as num?, step as num?);
-    } else {
-      current = null;
-    }
-
     return current;
-  }
-
-  static int moveNextDateTime(DateTime cur, DateTime? min, DateTime? max, Duration step) {
-    cur.add(step);
-
-    if ((max != null) && (cur.microsecondsSinceEpoch > max.microsecondsSinceEpoch)) {
-      cur = min;
-    }
-
-    return i;
-  }
-
-  static int moveNextInt(int i, int? min, int? max, int? step) {
-    i += (step ?? 1);
-
-    if ((max != null) && (i > max)) {
-      i = (min ?? 0);
-    }
-
-    return i;
-  }
-
-  static Object moveNextIterable(Iterable itr) {
-    final i = itr.iterator;
-    return (i.moveNext() ? i.current : itr.first);
-  }
-
-  static num moveNextNum(num i, num? min, num? max, num? step) {
-    i += (step ?? 1);
-
-    if ((max != null) && (i > max)) {
-      i = (min ?? 0);
-    }
-
-    return i;
   }
 
   Object? reset() {
     switch (type) {
       case PerfTestArgType.dateTime:
-    }
-    if (iterable != null) {
-      current = iterable!.first;
-    } else if (min is num?) {
-      current = min ?? 0;
-    } else {
-      current = null;
+        current = dateTimeMin;
+        break;
+      case PerfTestArgType.int:
+        current = intMin;
+        break;
+      case PerfTestArgType.iterable:
+        current = iterable.first;
+        break;
+      case PerfTestArgType.num:
+        current = numMin;
+        break;
+      default:
+        current = null;
     }
 
     return current;
